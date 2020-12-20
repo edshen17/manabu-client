8ed8928e628e35034f7b522e0655cdffceeb591a
Edwin <edshen17@avenues.org>
2020-12-19 19:46:27 -0500

    start teacher dashboard

--- src/components/layouts/LayoutDefault.vue
+++ src/components/layouts/LayoutDefault.vue
@@ -10,9 +10,6 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav class="ml-auto">
-            <!-- <b-nav-item v-for="route in navBarLinks" :key="route.id">
-              <b-link :to="route.link" class="nav-link menu-item" v-show="route.toShow" :class=" { active: $route.name == route.name } ">{{ route.displayName }}</b-link>
-            </b-nav-item> -->
             <b-nav-item>
               <b-link to="/teachers" class="nav-link menu-item" v-show="true" :class=" { active: $route.name == 'Teachers' } ">Find a Teacher</b-link>
             </b-nav-item>
@@ -61,7 +58,7 @@ export default {
   methods : {
     async getUserData() {
       // get token from local storage to use in routes 
-      let res = await axios.get('http://localhost:5000/api/me', { headers: {
+      const res = await axios.get('http://localhost:5000/api/me', { headers: {
           'x-access-token': localStorage.getItem('token'),
           'Accept' : 'application/json',
           'Content-Type': 'application/json'
