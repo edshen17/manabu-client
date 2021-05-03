12a63c362d6918bd0e7a6cb2bee31ae608c800f7
edshen17 <shen.ed@husky.neu.edu>
2021-05-03 19:11:40 -0400

    finish choose us

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,35 +19,37 @@
           <h1>{{ $t('homepage.chooseUs') }}</h1>
           <div class="block">
             <b-container fluid>
-            <b-row class="equal">
-              <b-col sm="6">
-                <div class='choose-us-parent'>
-                  <i class="fas fa-language fa-2x inline"></i>
-                  <h5 class="mx-2 my-1 bold">Customized Lessons</h5>
-                </div>
-                <p class="mt-2">Manabu teachers create personalized lesson plans that fit your learning styles and interests. Excercises and other materials are tailored specifically for you.</p>
-                <br />
-                <div class='choose-us-parent'>
-                  <i class="fas fa-chart-bar fa-2x inline"></i>
-                  <h5 class="mx-2 my-1 bold">Data Analytics</h5>
-                </div>
-                <p class="mt-2">Through Manabu, you can visualize your progress and growth. This can not only a way to stay motivated, but can also help you find areas of improvement.</p>
-              </b-col>
-              <b-col sm="6">
-                <div class='choose-us-parent'>
-                  <i class="fas fa-headphones fa-2x inline"></i>
-                  <h5 class="mx-2 my-1 bold">Immersion</h5>
-                </div>
-                <p class="mt-2">We focus on getting students into a habit of constant learning. We are confident that we can help you immerse in the language and achieve your fluency goals.</p>
-                <br />
-                <div class='choose-us-parent'>
-                  <i class="fas fa-hands-helping fa-2x inline"></i>
-                  <h5 class="mx-2 my-1 bold">Community</h5>
-                </div>
-                <p class="mt-2">Learning a language is difficult so if you ever feel stuck we are happy to guide you! We have a loving and supportive community of teachers and staff that will aid you throughout your studies.</p>
-              </b-col>
-            </b-row>
-          </b-container>
+              <b-row class="make-it-flex">
+                <b-col sm="6" class="flex-item-1">
+                  <div class='choose-us-parent'>
+                    <i class="fas fa-language fa-2x inline"></i>
+                    <h5 class="mx-2 my-1 bold">Customized Lessons</h5>
+                  </div>
+                  <p class="mt-2">Manabu teachers create personalized lesson plans that fit your learning styles and interests. Excercises and other materials are tailored specifically for you.</p>
+                </b-col>
+                <b-col sm="6" class="flex-item-1">
+                  <div class='choose-us-parent'>
+                    <i class="fas fa-headphones fa-2x inline"></i>
+                    <h5 class="mx-2 my-1 bold">Immersion</h5>
+                  </div>
+                  <p class="mt-2">We focus on getting students into a habit of constant learning. We are confident that we can help you immerse and achieve your fluency goals.</p>
+                </b-col>
+                <b-col sm="6" class="flex-item-1">
+                  <div class='choose-us-parent'>
+                    <i class="fas fa-chart-bar fa-2x inline"></i>
+                    <h5 class="mx-2 my-1 bold">Data Analytics</h5>
+                  </div>
+                  <p class="mt-2">Through Manabu, you can visualize your progress and growth. This can not only a way to stay motivated, but can also help you find areas of improvement.</p>
+                </b-col>
+                <b-col sm="6" class="flex-item-1">
+                  <div class='choose-us-parent'>
+                    <i class="fas fa-hands-helping fa-2x inline"></i>
+                    <h5 class="mx-2 my-1 bold">Community</h5>
+                  </div>
+                  <p class="mt-2">Learning a language can be difficult so if you ever feel stuck we are happy to guide you! We have a supportive community of teachers and staff that will aid you throughout your studies.</p>
+                </b-col>
+              </b-row>
+            </b-container>
           </div>
         </section>
       </div>
