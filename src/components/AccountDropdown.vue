96e496fb5fa979d1f846109bc4fc595c13ad784c
edshen17 <shen.ed@husky.neu.edu>
2021-09-06 18:11:52 -0500

    save accountDropdown component

---
+++ src/components/AccountDropdown.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="relative" @keydown.esc="showDropdown = false">
+    <button
+      class="relative z-10 block h-12 w-12 rounded-full overflow-hidden mx-4"
+      tabindex="0"
+      @click="showDropdown = !showDropdown"
+      @focusout="showDropdown = false"
+    >
+      <img
+        class="
+          h-full
+          w-full
+          object-cover
+          border-2 border-gray-600
+          focus:outline-none focus:border-white
+        "
+        src="https://lh3.googleusercontent.com/a-/AOh14GjQAkMXL_1lf8a8ymoxuR6PJZDhoVMNX8wUejGV=s96-c"
+      />
+    </button>
+    <div
+      v-show="showDropdown"
+      class="absolute right-0 mt-2 py-2 w-80 bg-gray-800 rounded-sm shadow-xl"
+    >
+      <ul>
+        <li class="px-4 py-2 text-white hover:text-white">ssa</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AccountDropdown',
+  components: {},
+  model: {},
+  props: {},
+  data() {
+    return {
+      showDropdown: false,
+    };
+  },
+  computed: {},
+  mounted() {
+    return;
+  },
+  methods: {},
+});
+</script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,11 +8,14 @@
       p-6
       lg:sticky
       w-full
-      z-10
+      z-20
       top-0
       bg-white
       shadow-md
+      relative
     "
+    tabindex="0"
+    @focusout="showMenu = false"
   >
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <router-link to="/" class="text-black no-underline"
@@ -45,19 +48,30 @@
       x-show.transition="true"
     >
       <ul class="pt-6 lg:pt-0 list-reset lg:flex justify-end flex-1 items-center">
-        <li
-          v-for="routerLink in routerLinks"
-          :key="routerLink.title"
-          class="mr-3"
-          @click="showMenu = !showMenu"
-        >
+        <li v-for="routerLink in routerLinks" :key="routerLink.title" @click="showMenu = !showMenu">
           <router-link
             v-show="routerLink.isShowing"
             :to="routerLink.link"
-            class="inline-block py-2 px-4 text-black no-underline"
+            class="inline-block py-3 md:py-2 px-4 text-black no-underline"
             >{{ routerLink.title }}</router-link
           >
         </li>
+        <account-dropdown class="hidden sm:block"></account-dropdown>
+        <div class="px-4 py-5 sm:hidden">
+          <div class="flex items-center">
+            <img
+              class="h-16 w-16 object-cover border-2 border-gray-600 rounded-full"
+              src="https://lh3.googleusercontent.com/a-/AOh14GjQAkMXL_1lf8a8ymoxuR6PJZDhoVMNX8wUejGV=s96-c"
+            />
+            <span class="ml-3 font-semi-bold"> Jane Doe </span>
+          </div>
+          <div class="">
+            <ul class="mt-4">
+              <li class="text-gray-400 mt-2">ssa</li>
+              <li class="text-gray-400 mt-2">ssa</li>
+            </ul>
+          </div>
+        </div>
       </ul>
     </div>
   </nav>
@@ -67,9 +81,13 @@
 import Vue from 'vue';
 import { mapGetters } from 'vuex';
 import { TranslateResult } from 'vue-i18n';
+import AccountDropdown from './AccountDropdown.vue';
 
 export default Vue.extend({
   name: 'Navbar',
+  components: {
+    AccountDropdown,
+  },
   data() {
     return {
       showMenu: false,
