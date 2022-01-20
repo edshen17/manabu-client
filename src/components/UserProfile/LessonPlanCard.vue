c9dbe6ebd2773b0693c3779e9f9941cf2c105574
Edwin <edshen17@avenues.org>
2022-01-20 15:51:29 -0500

    save lessonPlanCard component

--- src/components/UserProfile/LessonPlanCard.vue
+++ src/components/UserProfile/LessonPlanCard.vue
@@ -2,7 +2,14 @@
   <div class="rounded-lg shadow-md mt-5 h-auto pb-2 bg-white" data-app>
     <p class="justify-center p-6 mx-auto text-2xl">{{ $t('userProfile.teacher.lessonPlans') }}</p>
     <div v-for="pkg in visiblePackages" :key="pkg._id" class="px-6 mx-auto">
-      <v-dialog v-model="showDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-dialog
+        v-model="showDialog"
+        persistent
+        :retain-focus="false"
+        fullscreen
+        hide-overlay
+        transition="dialog-bottom-transition"
+      >
         <template v-slot:activator="{ on, attrs }">
           <button
             class="border-solid border-2 rounded-md mb-5 hover:bg-gray-100"
@@ -34,30 +41,57 @@
             </div>
           </button>
         </template>
-        <!-- <div class="bg-red-500 h-screen">
-          <div class="h-20 flex text-2xl w-full justify-center items-center bg-white font-bold">
-            <div>Choose your lesson plan</div>
-          </div>
-          <div class="flex justify-center items-center bg-green-500 flex-grow">sasa</div>
-        </div> -->
-        <div class="flex flex-col h-screen bg-white">
-          <div class="py-8 md:py-6 text-2xl font-bold border-b-2 border-gray-200">
-            <!-- <div class="flex">
-              <p>Choose your lesson plan</p>
-              <div class="float-right">sa</div>
-            </div> -->
-            <div class="flex static">
-              <h5 class="flex-1 text-center">Choose your lesson plan</h5>
-              <div class="absolute px-10">
-                <i class="fas fa-times cursor-pointer" @click="showDialog = false"></i>
+        <v-card>
+          <div class="flex flex-col h-screen bg-white">
+            <div class="py-8 md:py-6 text-2xl font-bold border-b-2 border-gray-200">
+              <div class="flex static">
+                <h5 class="flex-1 text-center">Choose your lesson plan</h5>
+                <div class="absolute px-10">
+                  <i class="fas fa-times cursor-pointer" @click="showDialog = false"></i>
+                </div>
+              </div>
+            </div>
+            <div class="mt-5">
+              <progress-bar :step-index="0" :step-total="4" />
+            </div>
+            <div class="flex justify-center items-center h-full bg-white">
+              <div>
+                <div
+                  v-for="dialogPkg in visiblePackages"
+                  :key="dialogPkg._id"
+                  class="w-2/6 mx-auto"
+                >
+                  <button class="border-solid border-2 rounded-md mb-5 hover:bg-gray-100">
+                    <div class="m-4 text-left">
+                      <p class="text-lg" :style="{ color: getRandomColor(dialogPkg.name) }">
+                        {{ getPackageTitle(dialogPkg) }}
+                      </p>
+                      <p class="mt-2 text-gray-600">{{ getPackageDescription(dialogPkg) }}</p>
+                      <div
+                        v-if="exchangeRates && exchangeRates.SGD"
+                        class="
+                          py-1
+                          mt-3
+                          w-24
+                          shadow-md
+                          bg-blue-400
+                          no-underline
+                          rounded-full
+                          text-white
+                          font-semibold
+                          text-sm text-center
+                        "
+                      >
+                        ~{{ getPackagePrice(dialogPkg.lessonAmount).toLocaleString() }}+
+                        {{ currency }}
+                      </div>
+                    </div>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-          <div class="mt-5">
-            <progress-bar :step-index="0" />
-          </div>
-          <div class="flex justify-center items-center flex-grow">sa</div>
-        </div>
+        </v-card>
       </v-dialog>
     </div>
   </div>
@@ -85,6 +119,10 @@ export default Vue.extend({
   data() {
     return {
       showDialog: false,
+      dialog: false,
+      notifications: false,
+      sound: true,
+      widgets: false,
     };
   },
   computed: {
