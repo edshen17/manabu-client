3b9d060e5c941adbc98860279fee7305a18766ac
Edwin Shen <edshen17@avenues.org>
2022-03-23 16:50:39 -0400

    save testimonials

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -96,15 +96,52 @@
         </div>
       </section>
       <section class="relative h-auto pb-10 lg:pb-12" style="background-color: #fdfbf6">
-        <div class="container mx-auto px-4 py-3">
-          <div class="flex flex-wrap text-center justify-center">
-            <div class="w-full lg:w-8/12">
-              <i class="fas fa-crown text-6xl text-red-400 my-5"></i>
-              <h2
-                class="text-2xl font-semibold text-center tracking-wide text-black capitalize lg:w-1/2 mx-auto"
-              >
-                #1 Online Japanese School in Singapore
-              </h2>
+        <div class="container mx-auto px-4 py-2">
+          <div class="flex flex-wrap text-center justify-center pt-6">
+            <div class="container px-5 mx-auto">
+              <h1 class="text-2xl font-semibold title-font text-gray-900 mb-9 text-center">
+                {{ $t('homepage.testimonials.title') }}
+              </h1>
+              <div class="flex flex-wrap">
+                <div
+                  v-for="testimonial in testimonialData"
+                  :key="testimonial.reviewer.name"
+                  class="mb-6 md:p-4 md:mb-0 md:w-1/2 w-full"
+                >
+                  <div
+                    class="h-full bg-white p-8 rounded shadow-lg transform duration-500 hover:-translate-y-1"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      class="block w-5 h-5 text-gray-400 mb-4"
+                      viewBox="0 0 975.036 975.036"
+                    >
+                      <path
+                        d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"
+                      ></path>
+                    </svg>
+                    <p class="leading-relaxed mb-6 text-left" style="min-height: 13em">
+                      {{ testimonial.review }}
+                    </p>
+                    <a class="inline-flex items-center">
+                      <img
+                        alt="testimonial"
+                        :src="testimonial.reviewer.imgSrc"
+                        class="w-16 h-16 rounded-full flex-shrink-0 object-cover object-center"
+                      />
+                      <span class="flex-grow flex flex-col pl-1 lg:pl-4">
+                        <span class="title-font font-medium text-gray-900">{{
+                          testimonial.reviewer.name
+                        }}</span>
+                        <span class="text-gray-500 text-sm">{{
+                          testimonial.reviewer.occupation
+                        }}</span>
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -165,6 +202,25 @@ export default Vue.extend({
             class: 'fas fa-hands-helping',
           },
         ];
+      },
+    },
+    testimonialData: {
+      get() {
+        const testimonialData = [];
+        for (let i = 0; i < 4; i++) {
+          const index = i + 1;
+          const translateKey = `homepage.testimonials.${index}`;
+          const testimonial = {
+            reviewer: {
+              name: this.$t(`${translateKey}.reviewer.name`),
+              occupation: this.$t(`${translateKey}.reviewer.occupation`),
+              imgSrc: require(`../assets/img/reviewer_${index}.jpg`),
+            },
+            review: this.$t(`${translateKey}.review`),
+          };
+          testimonialData.push(testimonial);
+        }
+        return testimonialData;
       },
     },
   },
